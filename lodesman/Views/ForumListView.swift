874c7a2ff131644f491220bb960e74dc2120b427
Dmitri Shuvalov <shuvalov.dmitri@outlook.com>
2022-01-23 12:07:33 +0200

    Добавляет `ForumListView`.

---
+++ lodesman/Views/ForumListView.swift
@@ -0,0 +1,32 @@
+//
+//  ForumListView.swift
+//  lodesman
+//
+//  Created by Dmitri Shuvalov on 23.01.2022.
+//
+
+import SwiftUI
+
+
+
+struct ForumListView: View
+{
+    let forums: [Forum]
+    @Binding var selection: Set<Int>
+
+    var body: some View {
+        List(forums, id: \.forumId, selection: $selection) { forum in
+            ForumRow(forum: forum)
+        }
+        .frame(minWidth: 250)
+    }
+}
+
+
+
+struct ForumListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForumListView(forums: ForumStub.preview, selection: .constant([]))
+            .frame(width: 400)
+    }
+}
