1574d8353a544af7bdd3e80ababa1de2a69abb2b
Dmitri Shuvalov <shuvalov.dmitri@outlook.com>
2022-01-23 12:02:05 +0200

    Добавляет в модель протокол `Forum`.

---
+++ lodesman/Model/Forum.swift
@@ -0,0 +1,33 @@
+//
+//  Forum.swift
+//  lodesman
+//
+//  Created by Dmitri Shuvalov on 23.01.2022.
+//
+
+import Foundation
+
+
+
+protocol Forum
+{
+    var forumId: Int { get }
+    var title: String { get }
+    var numberOfTopics: Int { get }
+}
+
+
+
+struct ForumStub: Forum
+{
+    var forumId: Int = 0
+    var title: String = "untitled"
+    var numberOfTopics: Int = 12_345
+
+    static let preview = "alpha beta gamma zeta"
+        .components(separatedBy: " ")
+        .enumerated()
+        .map{ (index, title) in
+            ForumStub(forumId: index, title: title, numberOfTopics: index * 9_876)
+        }
+}
