c9bace3541efea6d2e99a731d984a9b3acf201a9
Dmitri Shuvalov <shuvalov.dmitri@outlook.com>
2022-01-23 09:39:42 +0200

    Создаёт Xcode проект.

---
+++ lodesman/lodesmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  lodesmanApp.swift
+//  lodesman
+//
+//  Created by Dmitri Shuvalov on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct lodesmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
