074df3d0cbc93eb33e88f623d100c1ce3cb33c67
Dmitri Shuvalov <shuvalov.dmitri@outlook.com>
2022-01-24 09:58:51 +0200

    Добавляет `Catalog`

---
+++ lodesman/Model/Catalog.swift
@@ -0,0 +1,80 @@
+//
+//  Catalog.swift
+//  lodesman
+//
+//  Created by Dmitri Shuvalov on 24.01.2022.
+//
+
+import Foundation
+
+
+
+enum CatalogItemKind {
+    case section, forum
+}
+
+
+protocol CatalogItem
+{
+    var id: Int { get }
+    var kind: CatalogItemKind { get }
+    var title: String { get }
+    var children: [CatalogItem]? { get }
+}
+
+
+protocol Catalog
+{
+    var root: [CatalogItem] { get }
+}
+
+
+//MARK: - forEach
+extension CatalogItem
+{
+    func forEach(_ body: (CatalogItem) -> Void) {
+        for child in children ?? [] {
+            body(child)
+            child.forEach(body)
+        }
+    }
+}
+
+extension Catalog
+{
+    func forEach(_ body: (CatalogItem) -> Void) {
+        for item in root {
+            body(item)
+            item.forEach(body)
+        }
+    }
+}
+
+
+//MARK: -
+
+#if DEBUG
+struct CatalogItemStub: CatalogItem
+{
+    var id: Int
+    var kind: CatalogItemKind { id < 0 ? .section : .forum }
+    var title: String
+    var children: [CatalogItem]?
+}
+
+struct CatalogStub: Catalog
+{
+    var root: [CatalogItem]
+
+    static let preview = CatalogStub(root: [
+        CatalogItemStub(id: -11, title: "primary section", children: [
+            CatalogItemStub(id: 12, title: "alpha"),
+            CatalogItemStub(id: 13, title: "beta", children: [
+                CatalogItemStub(id: 14, title: "gamma from beta")]),
+            CatalogItemStub(id: 15, title: "zeta")]),
+        CatalogItemStub(id: -13, title: "secondary section", children: [
+            CatalogItemStub(id: 21, title: "alpha from secondary"),
+            CatalogItemStub(id: 22, title: "beta from secondary")])
+    ])
+}
+#endif
---
+++ lodesmanTests/CatalogTests.swift
@@ -0,0 +1,90 @@
+//
+//  CatalogTests.swift
+//  lodesmanTests
+//
+//  Created by Dmitri Shuvalov on 24.01.2022.
+//
+
+import XCTest
+@testable import lodesman
+
+
+fileprivate struct ItemStub: CatalogItem
+{
+    var id: Int = 0
+    var kind: CatalogItemKind = .section
+    var title: String = "untitled"
+    var children: [CatalogItem]?
+}
+
+fileprivate struct CatalogStub: Catalog
+{
+    var root: [CatalogItem] = []
+}
+
+
+class CatalogTests: XCTestCase
+{
+    func testForEachWithEmptyCatalog() throws {
+        var called = false
+        let catalog = CatalogStub()
+        catalog.forEach { _ in
+            called = true
+        }
+        XCTAssertFalse(called)
+    }
+
+    func testForEachWithOneEmptySection() throws {
+        var called = false
+        var result = [CatalogItem]()
+        let catalog = CatalogStub(root: [ItemStub(id: -1, kind: .section, title: "test")])
+        catalog.forEach { item in
+            called = true
+            result.append(item)
+        }
+        XCTAssertTrue(called)
+        XCTAssertEqual(result.count, 1)
+        let item = try XCTUnwrap(result.first)
+        XCTAssertEqual(item.id, -1)
+        XCTAssertEqual(item.kind, .section)
+        XCTAssertEqual(item.title, "test")
+        XCTAssertNil(item.children)
+    }
+
+    func testForEachWithTwoEmptySection() throws {
+        var called = false
+        let inputItems = [ItemStub(id: -1),
+                          ItemStub(id: -2)]
+        var result = [CatalogItem]()
+        let catalog = CatalogStub(root: inputItems)
+        catalog.forEach { item in
+            called = true
+            result.append(item)
+        }
+        XCTAssertTrue(called)
+        XCTAssertEqual(result.count, 2)
+        let firstItem = try XCTUnwrap(result.first)
+        XCTAssertEqual(firstItem.id, -1)
+        let lastItem = try XCTUnwrap(result.last)
+        XCTAssertEqual(lastItem.id, -2)
+    }
+
+    func testForEachWithOneSectionAndTwoNestingLevels() throws {
+        var called = false
+        let inputItems = [ItemStub(id: -1, children: [ItemStub(id: 1, children: [ItemStub(id: 2)])])]
+        var result = [CatalogItem]()
+        let catalog = CatalogStub(root: inputItems)
+        catalog.forEach { item in
+            called = true
+            result.append(item)
+        }
+        XCTAssertTrue(called)
+        XCTAssertEqual(result.count, 3)
+        let first = try XCTUnwrap(result[0])
+        XCTAssertEqual(first.id, -1)
+        let second = try XCTUnwrap(result[1])
+        XCTAssertEqual(second.id, 1)
+        let third = try XCTUnwrap(result[2])
+        XCTAssertEqual(third.id, 2)
+    }
+}
