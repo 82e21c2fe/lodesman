930ec0d0d2cb42117fdf5a5b3fed647264d849cf
Dmitri Shuvalov <shuvalov.dmitri@outlook.com>
2022-01-28 16:15:04 +0200

    Добавляет `TopicsView`

---
+++ lodesman/ContentViews/TopicsView.swift
@@ -0,0 +1,50 @@
+//
+//  TopicsView.swift
+//  lodesman
+//
+//  Created by Dmitri Shuvalov on 28.01.2022.
+//
+
+import SwiftUI
+
+
+
+struct TopicsView<Storage: TopicStorage>: View
+{
+    @ObservedObject var storage: Storage
+    let forums: Set<Int>
+    @Binding var selection: Set<Int>
+
+    @State private var filterText: String = ""
+    @State private var sortOrder: TopicSortOrder = .byLastUpdate
+
+    var body: some View {
+        TopicListView(storage.topics(fromForums: forums, whereTitleContains: filterText, sortedBy: sortOrder),
+                      groupRule: .init(sortOrder),
+                      selection: $selection)
+            .frame(minWidth: 250)
+            .toolbar {
+                ToolbarItemGroup(placement: .primaryAction) {
+                    TopicPinButton(storage: storage, selectedTopics: selection)
+                    Spacer()
+                    TopicSortOrderPicker(selection: $sortOrder)
+                        .pickerStyle(.segmented)
+                    Spacer()
+                }
+                ToolbarItem(placement: .principal) {
+                    TextField("filter", text: $filterText)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(minWidth: 250, idealWidth: 450, maxWidth: 600)
+                }
+            }
+
+    }
+}
+
+struct TopicsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopicsView(storage: TopicStorageStub(),
+                   forums: [1],
+                   selection: .constant([]))
+    }
+}
